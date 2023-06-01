646ccea0874bdfc742468026536f32e90c72dee7
DeShawn E <xeno2188@gmail.com>
2023-05-31 20:14:50 -0700

    Initial JS for updating the svg files

--- Web/index.js
+++ Web/index.js
@@ -1,8 +1,36 @@
+// TODO: Fix regex pattern for input. Console gave error
+// Funtoin if this is not defined ignore
 
+const hoursInputId = "timeInputHours";
+const minutesInputId = "timeInputMinutes";
+const secondsInputId = "timeInputSeconds";
+const hoursLeftDigit = "hoursLeft";
+const hoursRightDigit = "hoursRight";
+var hours;
+var minutes;
+var seconds;
+var timeInputFields;
 
+$(document).ready(function() {
+    timeInputFields = $(".input-time");
 
-function RefreshDigit() {
-    // Pass the ID of digits to refresh
-    // Get the number 
-    // Return the svg based on number
-};
+    // timeInputFields.change(function() {
+    //     var foo = document.getElementById(this.id).value;
+    // }); 
+    
+    timeInputFields.change(function(){
+        RefreshTimer(this);
+    });
+});
+
+function RefreshTimer(obj) {
+    // Get the values in each input field (array)
+    // For each value update the images
+    // Call function for what segments to load.
+    var timeValue;
+    var timeType;
+
+    if (obj !== undefined) {
+        timeValue = obj.value;
+    }
+}
